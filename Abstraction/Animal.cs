d1888351ca332ce73a687a448e30b9e4cd814f61
MasterRoshi11 <90256590+MasterRoshi11@users.noreply.github.com>
2021-09-08 21:38:41 +0545

    Add project files.

---
+++ Abstraction/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+     class Animal
+    {
+        public abstract void animalSound();
+
+        public void sleep()
+        {
+            Console.WriteLine("LULULU");
+
+        }
+}
